c420b1dc248678a4ba892e10e563a2e1f1631b4c
samuelkuria01 <samuel.kuria01@student.moringaschool.com>
2023-01-19 21:36:30 +0300

    add useEffect set up timeout to run after 1 sec clean up after timeout

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,9 +1,30 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
+useEffect(()=>{
+
+if(timeRemaining===0){
+  setTimeRemaining(10);
+  onAnswered(false);
+  return;
+}
+},[])
+
+  //set up a timeout to run after 1 second
+const timerId = setTimeout(()=>{
+  setTimeRemaining((timeRemaining)=>timeRemaining -1);
+}, [1000])
+
+  //clean up after the timeout
+  return function () {
+    clearTimeout(timerId);
+  }
+  
+
+  //run effect each time the timeremaining changes
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
